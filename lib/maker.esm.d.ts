c598a8ea334a54730152e284c6d23652f7ac3a07
wujianqi <janchie@163.com>
2019-06-09 16:03:27 +0800

    fix

--- lib/maker.esm.d.ts
+++ lib/maker.esm.d.ts
@@ -1,3 +1,3 @@
-import { maker, IMaker, MakerSetting, MethodFuction } from "./maker";
+import maker, { IMaker, MakerSetting, MethodFuction } from "./maker";
 export { MakerSetting, IMaker, MethodFuction };
 export default maker;
--- lib/validator.esm.d.ts
+++ lib/validator.esm.d.ts
@@ -1,3 +1,3 @@
-import { validator, IValidator, ValidSetting, RuleFunction, IChain } from "./validator";
+import validator, { IValidator, ValidSetting, RuleFunction, IChain } from "./validator";
 export { IValidator, ValidSetting, RuleFunction, IChain };
 export default validator;
